b4d600ead6b3e5738eac1986457e788ebef94acd
TheAryan77 <152590706+TheAryan77@users.noreply.github.com>
2025-04-13 22:35:44 +0530

    Update AuthForm.tsx

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -106,7 +106,7 @@ const AuthForm = ({ type }: { type: FormType }) => {
         </div>
 
         <h3>Practice job interviews with AI created by Aryan</h3>
-        <h4>sign up,first</h4>
+        <h4>SignUp first!!</h4>
 
         <Form {...form}>
           <form
