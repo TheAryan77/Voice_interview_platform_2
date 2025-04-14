eae122e9609132596f0b13d17ff5c72a72c6b127
TheAryan77 <152590706+TheAryan77@users.noreply.github.com>
2025-04-15 02:22:57 +0530

    made changes in navbar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,21 +2,23 @@ import Link from "next/link";
 import Image from "next/image";
 import { ReactNode } from "react";
 import { redirect } from "next/navigation";
-
+import { getCurrentUser } from "@/lib/actions/auth.action";
 import { isAuthenticated } from "@/lib/actions/auth.action";
 
 const Layout = async ({ children }: { children: ReactNode }) => {
   const isUserAuthenticated = await isAuthenticated();
   if (!isUserAuthenticated) redirect("/sign-in");
+  const user = await getCurrentUser();
 
   return (
     <div className="root-layout">
-      <nav>
-        <Link href="/" className="flex items-center gap-2">
-          <Image src="/logo.svg" alt="MockMate Logo" width={38} height={32} />
-          <h2 className="text-primary-100">DevHire by Aryan</h2>
-        </Link>
-      </nav>
+      <nav className="flex items-center justify-between px-4 py-2">
+  <Link href="/" className="flex items-center gap-2">
+    <Image src="/logo.svg" alt="MockMate Logo" width={38} height={32} />
+    <h2 className="text-primary-100">DevHire by Aryan</h2>
+  </Link>
+  <h2 className="text-primary-100 ml-auto">Hey <span className="waving-hand">&#9995;</span> {user?.name}</h2>
+</nav>
 
       {children}
     </div>
